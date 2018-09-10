f938ea9b88e7567356a13e239f56986e652d8912
ke2933 <534144810@qq.com>
2018-09-10 15:57:27 +0800

    获取文件路径方法提取

---
+++ js/GetObjectURL.js
@@ -0,0 +1,12 @@
+//获取预览图片路径
+var getObjectURL = function (file) {
+    var url = null;
+    if (window.createObjectURL != undefined) { // basic
+        url = window.createObjectURL(file);
+    } else if (window.URL != undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file);
+    } else if (window.webkitURL != undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file);
+    }
+    return url;
+}

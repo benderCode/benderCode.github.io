b8f5791755edf7c0f04f08fb53499c36c08d2260
ke2933 <1374512893@qq.com>
2019-01-22 15:00:59 +0800

    插件整理

--- reactnative/reactnative.js
+++ reactnative/reactnative.js
@@ -1,12 +1,13 @@
 var dependencies = {
     "react-native-communications": "^2.2.1",// linking 调用第三方应用
     "react-native-fs": "^0.10.8",// 操作文件
-    "react-native-image-picker": "^0.26.10",// 上传图片:单张
-    "react-native-image-crop-picker": "^0.21.3",// 上传图片:多张/裁剪
-    "react-native-img-cache": "^1.5.3",// 图片缓存 需要 react-native-fs
+    "rn-fetch-blob": "^0.10.15",// 数据传输操作
+    "react-native-image-picker": "^0.26.10",// 获取图片:单张
+    "react-native-image-crop-picker": "^0.21.3",// 获取图片:多张/裁剪
+    "react-native-img-cache": "^1.5.3",// 图片缓存 需要 rn-fetch-blob
     "react-native-iphone-x-helper": "^1.0.3",// 判断iPhoneX
     "react-native-linear-gradient": "^2.4.0",// 渐变
-    "react-native-qrcode-svg": "^0.2.7",// 生成二维码
+    "react-native-qrcode-svg": "^0.2.7",// 生成二维码 需要 react-native-svg
     "react-native-svg": "^6.5.2",// svg
     "react-native-shadow": "^1.2.2",// 盒子阴影 需要 react-native-svg
     "react-native-smscode-count-down": "^0.1.14",// 短信倒计时
@@ -21,11 +22,11 @@ var dependencies = {
     "react-native-signature-capture": "^0.4.9",// 签名
     "react-native-sound": "^0.10.4",// 播放录音
     "react-native-banner-carousel": "^1.0.2",// 轮播图
+    "react-native-swiper": "^1.5.14",// 轮播图  android竖向bug
     "react-native-background-timer": "^2.0.1",// 锁屏倒计时
     "react-native-datepicker": "^1.7.2",// 日期时间选择器
     "react-native-scrollable-tab-view": "^0.10.0",// 多tab切换
     "react-native-swipeout": "^2.3.6",// 侧滑按钮
-    "react-native-swiper": "^1.5.14",// 轮播图  android竖向bug
     "react-native-view-shot": "^2.5.0",// 截屏
     "react-native-parallax-scroll-view": "0.21.3",// 下拉放大
 }

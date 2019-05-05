80614a9e9081f1cef8733db3fe235f8ac6312c28
ke2933 <1374512893@qq.com>
2019-05-05 11:28:36 +0800

    Create Authority.js
    常用权限整理

---
+++ reactnative/Authority.js
@@ -0,0 +1,65 @@
+/** 
+ * 常用权限
+*/
+// IOS
+<key>NSCameraUsageDescription</key>
+<string>App需要您的同意,才能访问相机</string>
+
+<key>NSPhotoLibraryUsageDescription</key>
+<string>App需要您的同意,才能访问相册</string>
+
+<key>NSPhotoLibraryAddUsageDescription</key>
+<string>App需要您的同意,才能保存图片到您的相册</string>
+
+<key>NSContactsUsageDescription</key>
+<string>App需要您的同意,才能访问通讯录</string>
+
+<key>NSLocationAlwaysUsageDescription</key>
+<string>App需要您的同意,才能始终访问位置</string>
+
+<key>NSLocationUsageDescription</key>
+<string>App需要您的同意,才能访问位置</string>
+
+<key>NSLocationWhenInUseUsageDescription</key>
+<string>App需要您的同意,才能在使用期间访问位置</string>
+
+<key>NSMicrophoneUsageDescription</key>
+<string>App需要您的同意,才能访问麦克风</string>
+
+<key>NSBluetoothPeripheralUsageDescription</key>
+<string>App需要您的同意,才能访问蓝牙</string>
+
+<key>NSCalendarsUsageDescription</key>
+<string>App需要您的同意,才能访问日历</string>
+
+<key>NSAppleMusicUsageDescription</key>
+<string>App需要您的同意,才能访问媒体资料库</string>
+
+<key>NSHealthShareUsageDescription</key>
+<string>App需要您的同意,才能访问健康分享</string>
+
+<key>NSHealthUpdateUsageDescription</key>
+<string>App需要您的同意,才能访问健康更新</string>
+
+<key>NSMotionUsageDescription</key>
+<string>App需要您的同意,才能访问运动与健身</string>
+
+<key>NSRemindersUsageDescription</key>
+<string>App需要您的同意,才能访问提醒事项</string>
+
+
+// Android
+
+<!--访问网络权限-->
+<uses-permission android:name="android.permission.INTERNET" />
+<uses-permission android:name="android.permission.SYSTEM_ALERT_WINDOW"/>
+<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+<uses-permission android:name="android.permission.CAMERA"/>
+<uses-permission android:name="android.permission.VIBRATE" />
+<uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES" />
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+<uses-permission android:name="android.permission.READ_PHONE_STATE" />
+<uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />

a240896fede0988d9a50332a3156f14bda6da0b4
ke2933 <1374512893@qq.com>
2019-06-05 20:23:14 +0800

    Fetch 超时处理

--- js/http/Fetch.js
+++ js/http/Fetch.js
@@ -33,3 +33,34 @@ function fetchGet(requestUrl, headers) {
 }
 let result = fetchPost("url", { "token": "token" }, {});
 let result = fetchGet("url", { "token": "token" });
+
+// Fetch 超时处理
+export default function Post(url, data = {}, time = 20000) {
+    let result = new Promise((resolve, reject) => {
+        let requestOption = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: paramJson,
+            credentials: 'include',// 是否携带 cookie include-携带 same-origin-同源请求才发送cookie omit-缺省值, 默认为该值
+        }
+        fetch(url, requestOption)
+            .then((response) => response.json())
+            .then((responseData) => {
+                resolve(responseData)
+            })
+            .catch((error) => {
+                console.log('接口请求错误信息', error);
+            })
+    })
+
+    let timeout = new Promise(function (resolve, reject) {
+        setTimeout(() => { resolve({ status: 'NONETWORK', msg: '请求超时' }) }, time)
+    });
+
+    return Promise.race([
+        result, // 正常访问接口返回的数据
+        timeout, // 超过请求时间时返回 "timeout"
+    ]);
+}

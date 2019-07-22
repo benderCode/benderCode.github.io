381cb7fd4c80ef67f08632d07dce05783b587cdb
ke2933 <534144810@qq.com>
2019-07-22 11:32:30 +0800

    tingyun

---
+++ reactnative/TingYun.js
@@ -0,0 +1,7 @@
+// 听云
+// 接入文档 https://doc.tingyun.com/app/html/firstusing.html
+
+// 安卓图片加载显示bug
+// 创建 tingyun.properties 文件
+// 内容如上 noInstrumentedClasses=com/facebook,com/reactnative
+// 复制到  项目app（主模块）目录 和项目根目录（android目录）  各一份
